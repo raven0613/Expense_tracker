8800913c502b000e8fa2fcdc2e9c98199d6a3c07
Raven <reevan0613@gmail.com>
2022-08-24 16:22:56 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+require('dotenv').config();
+const app = express();
+const exphdb = require('express-handlebars');
+const PORT = process.env.PORT;
+const routes = require('./routes');
+
+
+app.engine('handlebars' , exphdb.engine({ defaultLayout : 'main' }));
+app.set('view engine' , 'handlebars');
+
+app.use(express.static('public'))
+app.use(routes);
+
+
+
+app.listen(PORT , (req , res) => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
