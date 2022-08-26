2b0813502451d3df7eab2d89953f3779f070682c
Raven <reevan0613@gmail.com>
2022-08-26 15:33:27 +0800

    feat: add register router

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const home = require('./modules/home')
 const records = require('./modules/records')
+const user = require('./modules/user');
 
-router.use('/' , home);
 router.use('/records' , records);
+router.use('/users' , user);
+router.use('/' , home);
+
 
 
 
---
+++ routes/modules/user.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const router = express.Router();
+const User = require('../../models/user');
+
+
+
+//get /users/login
+router.get('/login' , (req , res) => {
+  res.render('login')
+})
+
+//get /users/register
+router.get('/register' , (req , res) => {
+  res.render('register')
+})
+
+//post /users/register
+router.post('/register' , (req , res) => {
+  const {name , email , password , confirmedPassword} = req.body
+  
+  //如果user是登入狀態那就強制導回首頁
+
+  //確認密碼
+  if (confirmedPassword !== password) {
+    return console.log('密碼不符')
+  }
+  //確認資料庫內的user數量
+  User.find()
+    .then(user => {
+      const userCount = user.length;
+      return userCount;
+    })
+    .then(userCount => {
+      
+      User.findOne({ email })
+        .then(user => {
+          if (!user){
+            return bcrypt.genSalt(10)
+              .then(salt => bcrypt.hash(password , salt))
+              .then(hash => {
+                return User.create({ id : userCount + 1 , name , email , password : hash })
+              })
+              .then(() => res.redirect('/'))
+              .catch(err => console.log(err))
+          }
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+
+
+})
+
+//post /users/login
+
+
+//get /users/logout
+
+
+module.exports = router;
